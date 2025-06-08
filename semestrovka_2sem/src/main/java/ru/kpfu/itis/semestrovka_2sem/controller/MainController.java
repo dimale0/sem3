52b4c2ffe48f66d715e43ec552cda140ab3e13e6
Dmitry Leontev <145154809+dimale0@users.noreply.github.com>
2025-06-08 17:13:16 +0300

    Fix tutor request creation

---
+++ semestrovka_2sem/src/main/java/ru/kpfu/itis/semestrovka_2sem/controller/MainController.java
@@ -0,0 +1,13 @@
+package ru.kpfu.itis.semestrovka_2sem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
