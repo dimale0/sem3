5c9a94a723cd2b9faf644c02c15839a9790543be
Dmitry Leontev <145154809+dimale0@users.noreply.github.com>
2025-06-08 15:16:41 +0300

    Fix tutor request creation

--- semestrovka_2sem/src/main/java/ru/kpfu/itis/semestrovka_2sem/dto/TutorCreateDto.java
+++ semestrovka_2sem/src/main/java/ru/kpfu/itis/semestrovka_2sem/dto/TutorCreateDto.java
@@ -1,6 +1,7 @@
 package ru.kpfu.itis.semestrovka_2sem.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -21,6 +22,10 @@ public class TutorCreateDto {
     @Size(max = 1000, message = "Описание максимум 1000 символов")
     private String description;
 
-    @NotNull(message = "Набор предметов не может быть null")
-    private Set<Long> subjectIds;
+    @NotEmpty(message = "Нужно выбрать хотя бы один предмет")
+    private Set<Long> subjectIds = new java.util.HashSet<>();
+
+    /** Дополнительный предмет, если его нет в списке */
+    @Size(max = 100, message = "Название предмета максимум 100 символов")
+    private String newSubjectName;
 }
